a462a0483f654f02c3fbf41158c6fe1f89136b80
Raghav <raghavthakar12@gmail.com>
2021-05-01 12:54:32 +0530

    Make rudimentary conveyor model, rudimentary plugin

--- src/chipsy_description/src/ConveyorBeltPlugin.hh
+++ src/chipsy_description/src/ConveyorBeltPlugin.hh
@@ -1,88 +1,30 @@
-#ifndef _GAZEBO_CONVEYOR_BELT_PLUGIN_HH_
-#define _GAZEBO_CONVEYOR_BELT_PLUGIN_HH_
-
-#include <gazebo/common/Event.hh>
-#include <gazebo/common/Plugin.hh>
-#include <ignition/math/Angle.hh>
-#include <gazebo/physics/Joint.hh>
-#include <gazebo/physics/Link.hh>
-#include <gazebo/physics/Model.hh>
-#include <gazebo/transport/transport.hh>
-#include <gazebo/util/system.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
 #include <sdf/sdf.hh>
 
 namespace gazebo
 {
-  class GAZEBO_VISIBLE ConveyorBeltPlugin : public ModelPlugin
+  class ConveyorBeltPlugin : public ModelPlugin
   {
-    /// \brief Constructor.
-    public: ConveyorBeltPlugin() = default;
+  //Constructor
+  public: ConveyorBeltPlugin() = default;
 
-    /// \brief Destructor.
-    public: virtual ~ConveyorBeltPlugin();
+  //Destructor
+  public: virtual ~ConveyorBeltPlugin();
 
-    /// \brief Load the model plugin.
-    /// \param[in] _model Pointer to the model that loaded this plugin.
-    /// \param[in] _sdf SDF element that describes the plugin.
-    public: virtual void Load(physics::ModelPtr _model,
-                              sdf::ElementPtr _sdf);
+  //Mandatory Load method
+  public: virtual void Load(physics::ModelPtr _model,
+                            sdf::ElementPtr _sdf);
 
-    /// \brief Callback that receives the world update event
-    protected: void OnUpdate();
+  protected: void OnUpdate();
 
-    /// \brief Get if the belt is enabled.
-    /// \return True if enabled.
-    protected: bool IsEnabled() const;
+  /// \brief Pointer to the update event connection.
+  private: event::ConnectionPtr updateConnection;
 
-    /// \brief Get the power of the conveyor belt.
-    /// \return Power of the belt as a percentage (0-100).
-    protected: double Power() const;
+  //Stores name of belt joint
+  private: std::string belt_joint_name;
 
-    /// \brief Set the power of the conveyor belt.
-    /// \param[in] _power Power of the belt as a percentage (0-100).
-    protected: void SetPower(const double _power);
-
-    /// \brief Overwrite this method for sending periodic updates with the
-    /// conveyor state.
-    private: virtual void Publish() const;
-
-    /// \brief Call back for enable/disable messaged.
-    protected: void OnEnabled(ConstGzStringPtr &_msg);
-
-    /// \brief Belt velocity (m/s).
-    protected: double beltVelocity = 0.0;
-
-    /// \brief Belt power expressed as a percentage of the internal maximum
-    /// speed.
-    protected: double beltPower = 0.0;
-
-    /// \brief If true, power commands are processed, otherwise the belt won't move.
-    protected: bool enabled = true;
-
-    /// \brief Pointer to the update event connection.
-    private: event::ConnectionPtr updateConnection;
-
-    /// \brief The joint that controls the movement of the belt.
-    private: physics::JointPtr joint;
-
-    /// \brief The belt's link.
-    private: physics::LinkPtr link;
-
-    /// \brief When the joint reaches this point, it will go back to its initial
-    /// position.
-    private: double limit;
-
-    /// \brief Maximum linear velocity of the belt.
-    private: const double kMaxBeltLinVel = 0.2;
-
-    /// \brief Gazebo node for communication.
-    protected: transport::NodePtr gzNode;
-
-    /// \brief Gazebo publisher for modifying the rate of populating the belt.
-    public: transport::PublisherPtr populationRateModifierPub;
-
-    /// \brief Gazebo subscriber for modifying the enabled state of the belt.
-    public: transport::SubscriberPtr enabledSub;
+  //Stores a shared pointer to the belt joint
+  private: physics::JointPtr belt_joint;
   };
 }
-#endif
